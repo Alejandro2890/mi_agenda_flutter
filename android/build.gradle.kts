c407b0b2d304cc1d5d400727720e4ecb29983118
Alejandro2890 <144248224+Alejandro2890@users.noreply.github.com>
2026-04-26 16:56:09 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,12 @@
 buildscript {
-    extra.apply {
-        set("kotlin_version", "2.0.0")
-    }
+    val kotlin_version = "2.0.0"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra.get("kotlin_version")}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
